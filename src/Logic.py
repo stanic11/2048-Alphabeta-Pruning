52e56c752c83ef9d4856142e00a3bf677e6ae3e2
YNAG <109913898+stanic11@users.noreply.github.com>
2023-10-22 10:48:09 +0800

    Add files via upload

--- src/Logic.py
+++ src/Logic.py
@@ -1,108 +1,179 @@
+import os
 import random
+import xml.etree.ElementTree as ET
+
 import constant as c
 
-# TODO: 每次合并一个方块即停止循环
+
 # 已完成内容：
 # fst - 初始化矩阵储存各点数值
 # sec - 在矩阵内随机两点生成2与4中的随机数值
 # thd - 实现上下左右移动方块
-# fourth - 每次移动后在空余位置生成一个方块
+# 4th - 每次移动后在空余位置生成一个方块
+# TODO: 提示功能
+
+class game:
+    def __init__(self):
+        # 初始化游戏
+        self.matrix = Matrix()
+        self.initTwoRandomGrid()
+        # 初始化积分列表
+        self.root = ET.Element("data")
+        self.tree = ET.ElementTree(self.root)
+
+    def __XML__init__(self):
+        if os.path.exists(c.xmlFile):
+            return
+        else:
+            return
+
+    def initTwoRandomGrid(self):
+        # 随机在矩阵中找寻两个格子生成随机数
+        # 若两次生成的为同一格子，则进入while死循环，直到生成的格子不同为止
+        col = random.randint(0, 3)
+        row = random.randint(0, 3)
+        newCol = random.randint(0, 3)
+        newRow = random.randint(0, 3)
+        # 防止生成在同一格子内
+        while newCol == col and newRow == row:
+            newCol = random.randint(0, 3)
+            newRow = random.randint(0, 3)
+        self.matrix.gridChange(col, row, random.choice([2, 4]))
+        self.matrix.gridChange(newCol, newRow, random.choice([2, 4]))
+
+    def restart(self):
+        # 按下按键之后
+        self.__init__()
+
+    def gameState(self):
+        # 检测是否胜利
+        # 每次移动之后再进行检测，以此节约CPU资源
+        zeroCount = 0
+        for i in range(c.COLUMN):
+            for j in range(c.ROW):
+                if self.matrix.getMatrix()[i][j] == 2048:
+                    return 'win'
+                if self.matrix.getMatrix()[i][j] == 0:
+                    zeroCount += 1
+        if zeroCount == 0:
+            return 'game over'
+        # 数零个数，若零个数为0 说明格子全部占满，游戏失败
+
+    def pointsList(self):
+        file = open('rank.xml', 'r')
+
+    def gameTips(self):
+        # 按下按钮 输出提示
+
+        return
 
 
-def initGame():
-    matrix = []
-    # Create a 4x4 matrix to store the grid variable.
-    for i in range(4):
-        matrix.append([0] * 4)
-    initTwoRandomGrid(matrix)
-    return matrix
+class Matrix:
+    def __init__(self):
+        self.matrix = []
+        # 初始化一个4x4的矩阵用于储存数据
+        for i in range(c.COLUMN):
+            self.matrix.append([0] * c.ROW)
 
+    def getMatrix(self):
+        return self.matrix
 
-def initTwoRandomGrid(matrix):
-    col = random.randint(0, 3)
-    row = random.randint(0, 3)
-    matrix[col][row] = random.choice([2, 4])
-    col__ = random.randint(0, 3)
-    row__ = random.randint(0, 3)
-    isRowColSame(matrix, row, row__, col, col__)
+    def rewriteMatrix(self,matrix):
+        self.matrix = matrix
 
+    def gridChange(self, i, j, value):
+        self.matrix[i][j] = value
 
-def isRowColSame(matrix, row, row__, col, col__):
-    if row != row__ or col != col__:
-        matrix[col__][row__] = random.choice([2, 4])
-    else:
-        col__ = random.randint(0, 3)
-        row__ = random.randint(0, 3)
-        isRowColSame(matrix, row, row__, col, col__)
-
-
-def left(matrix):
-    # 矩阵左移操作
-    for j in range(c.COLUMN - 1, 0, -1):
+    ########################################################################
+    def left(self):
+        # 矩阵左移操作
         for i in range(c.ROW):
-            if matrix[i][j] != 0 and j > 0:
-                if matrix[i][j - 1] == 0:
-                    matrix[i][j - 1] = matrix[i][j]
-                    matrix[i][j] = 0
-                elif matrix[i][j - 1] == matrix[i][j]:
-                    matrix[i][j - 1] *= 2
-                    matrix[i][j] = 0
-    createGrid(matrix)
+            # 抽出矩阵的每一行
+            self.matrix[i] = self.matrixChange(self.matrix[i], 'left')
+        self.createGrid(self.matrix)
+        return self
 
-
-def right(matrix):
-    for j in range(0, c.COLUMN - 1):
+    def right(self):
         for i in range(c.ROW):
-            if matrix[i][j] != 0 and j < c.COLUMN:
-                if matrix[i][j + 1] == 0:
-                    matrix[i][j + 1] = matrix[i][j]
-                    matrix[i][j] = 0
-                elif matrix[i][j + 1] == matrix[i][j]:
-                    matrix[i][j + 1] *= 2
-                    matrix[i][j] = 0
-    createGrid(matrix)
+            # 抽出矩阵的每一行
+            self.matrix[i] = self.matrixChange(self.matrix[i], 'right')
+        self.createGrid(self.matrix)
+        return self
 
-
-def up(matrix):
-    # 进行 行变换
-    for i in range(c.ROW - 1, 0, -1):
+    def up(self):
+        # 进行 行变换
         for j in range(c.COLUMN):
-            if matrix[i][j] != 0 and i < c.ROW:
-                if matrix[i - 1][j] == 0:
-                    matrix[i - 1][j] = matrix[i][j]
-                    matrix[i][j] = 0
-                elif matrix[i - 1][j] == matrix[i][j]:
-                    matrix[i - 1][j] *= 2
-                    matrix[i][j] = 0
-    createGrid(matrix)
+            tmpMatrix = []
+            for i in range(c.ROW):
+                # 抽出矩阵的每一列
+                tmpMatrix.append(self.matrix[i][j])
+            tmpMatrix = self.matrixChange(tmpMatrix, 'up')
+            # 将临时矩阵的值返回到原矩阵
+            for i in range(c.ROW):
+                self.matrix[i][j] = tmpMatrix[i]
+        self.createGrid(self.matrix)
+        return self
 
-
-def down(matrix):
-    for i in range(0, c.ROW - 1):
+    def down(self):
+        # 进行 行变换
         for j in range(c.COLUMN):
-            if matrix[i][j] != 0 and i < c.ROW:
-                if matrix[i + 1][j] == 0:
-                    matrix[i + 1][j] = matrix[i][j]
-                    matrix[i][j] = 0
-                elif matrix[i + 1][j] == matrix[i][j]:
-                    matrix[i + 1][j] *= 2
-                    matrix[i][j] = 0
-    createGrid(matrix)
+            tmpMatrix = []
+            for i in range(c.ROW):
+                # 抽出矩阵的每一列
+                tmpMatrix.append(self.matrix[i][j])
+            tmpMatrix = self.matrixChange(tmpMatrix, 'down')
+            # 将临时矩阵的值返回到原矩阵
+            for i in range(c.ROW):
+                self.matrix[i][j] = tmpMatrix[i]
+        self.createGrid(self.matrix)
+        return self
 
+    ########################################################################
 
-def createGrid(matrix):
-    spaceList = []
-    for i in range(c.COLUMN):
-        for j in range(c.ROW):
-            if matrix[i][j] == 0:
-                spaceList.append([i, j])
-    tmp = random.choice(spaceList)
-    matrix[tmp[0]][tmp[1]] = random.choice([2, 4])
+    def createGrid(self, matrix):
+        spaceList = []
+        for i in range(c.COLUMN):
+            for j in range(c.ROW):
+                if matrix[i][j] == 0:
+                    spaceList.append([i, j])
+        if len(spaceList) != 0:
+            tmp = random.choice(spaceList)
+            matrix[tmp[0]][tmp[1]] = random.choice([2, 4])
 
+    def matrixChange(self, matrix, front):
+        newMatrixRow = []
+        count = 0
+        for i in range(c.COLUMN):
+            if matrix[i] != 0:
+                newMatrixRow.append(matrix[i])
+            else:
+                count += 1
+        newMatrixRow = self.merge(newMatrixRow, front)
+        if front == 'left' or front == 'up':
+            for j in range(count):
+                newMatrixRow.append(0)
+        else:
+            for j in range(count):
+                newMatrixRow.insert(0, 0)
+        return newMatrixRow
 
-def isWinner(matrix):
-    # 注：该函数应放置在死循环中
-    for i in range(c.COLUMN):
-        for j in range(c.ROW):
-            if matrix[i][j] == 2048:
-                return True
+    def merge(self, matrix, front):
+        if front == 'left' or front == 'up':
+            for i in range(len(matrix) - 1):
+                if matrix[i] == matrix[i + 1] and matrix[i] != 0:
+                    matrix[i] *= 2
+                    matrix[i + 1] = 0
+                    i += 1  # 跳到下一个非零元素
+                elif matrix[i] == 0 and matrix[i + 1] != 0:
+                    matrix[i] = matrix[i + 1]
+                    matrix[i + 1] = 0
+        if front == 'right' or front == 'down':
+            for i in range(len(matrix) - 1, 0, -1):
+                if matrix[i] == matrix[i - 1] and matrix[i] != 0:
+                    matrix[i] *= 2
+                    matrix[i - 1] = 0
+                    i -= 1  # 跳到下一个非零元素
+                elif matrix[i] == 0 and matrix[i - 1] != 0:
+                    matrix[i] = matrix[i - 1]
+                    matrix[i - 1] = 0
+        return matrix
--- src/ai.py
+++ src/ai.py
@@ -1,120 +1,217 @@
 import math
+
 import Logic
 import constant as c
 
 
-def getSmoothness(matrix):
-    # 计算每格平滑度
-    smoothness = 0
-    # 先对每一行进行单独求解
-    for i in range(c.ROW):
-        tmpMatrix = matrix[i]
-        for j in range(len(tmpMatrix) - 1):
-            if tmpMatrix[j] != 0:
-                for k in range(j + 1, len(tmpMatrix)):
-                    if tmpMatrix[k] != 0:
-                        smoothness -= math.fabs(tmpMatrix[k] - tmpMatrix[j])
-                        break
-
-    # 对每一列求解
-    for j in range(c.COLUMN):
-        tmpMatrix = []
-        for i in range(c.ROW):
-            # 抽出矩阵的每一列
-            tmpMatrix.append(matrix[i][j])
-        for k in range(len(tmpMatrix) - 1):
-            if tmpMatrix[k] != 0:
-                for t in range(k + 1, len(tmpMatrix)):
-                    if tmpMatrix[t] != 0:
-                        smoothness -= math.fabs(tmpMatrix[k] - tmpMatrix[t])
-                        break
-
-    return smoothness
-
-
-def getMonotonicity(matrix):
-    scoreList = [0, 0, 0, 0]
-    # Right
-    for i in range(c.ROW):
-        tmpMatrix = matrix[i]
-        for j in range(len(tmpMatrix) - 1):
-            if tmpMatrix[j] != 0:
-                for k in range(j + 1, len(tmpMatrix)):
-                    if tmpMatrix[k] != 0:
-                        if tmpMatrix[j] >= tmpMatrix[k]:
-                            scoreList[0] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[j]) / math.log(2)
-                        else:
-                            scoreList[1] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[j]) / math.log(2)
-    # Left
-    for i in range(c.ROW):
-        tmpMatrix = matrix[i]
-        for j in range(len(tmpMatrix) - 1, 0, -1):
-            if tmpMatrix[j] != 0:
-                for k in range(j - 1, len(tmpMatrix), -1):
-                    if tmpMatrix[k] != 0:
-                        if tmpMatrix[j] >= tmpMatrix[k]:
-                            scoreList[0] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[j]) / math.log(2)
-                        else:
-                            scoreList[1] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[j]) / math.log(2)
-
-    # Up
-    for j in range(c.COLUMN):
-        tmpMatrix = []
-        for i in range(c.ROW):
-            # 抽出矩阵的每一列
-            tmpMatrix.append(matrix[i][j])
-        for k in range(len(tmpMatrix) - 1):
-            if tmpMatrix[k] != 0:
-                for t in range(k + 1, len(tmpMatrix)):
-                    if tmpMatrix[t] != 0:
-                        if tmpMatrix[k] >= tmpMatrix[t]:
-                            scoreList[2] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[t]) / math.log(2)
-                        else:
-                            scoreList[3] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[t]) / math.log(2)
-    # Down
-    for j in range(c.COLUMN):
-        tmpMatrix = []
-        for i in range(c.ROW):
-            # 抽出矩阵的每一列
-            tmpMatrix.append(matrix[i][j])
-        for k in range(len(tmpMatrix) - 1, 0, -1):
-            if tmpMatrix[k] != 0:
-                for t in range(k - 1, len(tmpMatrix), -1):
-                    if tmpMatrix[t] != 0:
-                        if tmpMatrix[k] >= tmpMatrix[t]:
-                            scoreList[2] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[t]) / math.log(2)
-                        else:
-                            scoreList[3] += math.log(tmpMatrix[k]) / math.log(2) - math.log(tmpMatrix[t]) / math.log(2)
-    return max(scoreList[0], scoreList[1]) + max(scoreList[2], scoreList[3])
-
-
-def getEmptyCounts(matrix):
-    counts = 0
-    for i in range(c.ROW):
-        for j in range(c.COLUMN):
-            if matrix[i][j] == 0:
-                counts += 1
-    return counts
-
-
-def getMaxValue(matrix):
-    MAX = matrix[0][0]
-    for i in range(c.ROW):
-        for j in range(c.COLUMN):
-            if matrix[i][j] >= MAX:
-                MAX = matrix[i][j]
-    return MAX
-
-
 class Ai:
-    __smoothWeight = 0.1
-    __emptyWeight = 2.7
-    __maxWeight = 1.0
-    __monoWeight = 1.0  # 单调性权重
+    def __init__(self):
+        self.GAME = Logic.game()
+        self.alpha = -math.inf
+        self.beta = +math.inf
+        self.headNode = Node(self.GAME.matrix, 1)
+        self.depth = 6
+        #########################
+        self.smoothWeight = 0.1
+        self.emptyWeight = 2.7
+        self.maxWeight = 1.0
+        self.monoWeight = 1.0  # 单调性权重
+        ##########################
+
+    def alphaBetaSearch(self, node, depth, minimaxPlayer):
+        if depth == 0 or self.GAME.gameState() == 'win':
+            return node, self.eval(node.matrix.getMatrix())  # 返回节点和对应的值
+
+        if minimaxPlayer:
+            maxEval = -math.inf
+            bestChild = None  # 用于记录最大值的子节点
+            for child in node.sonNode:
+                childNode, eValue = self.alphaBetaSearch(child, depth - 1, False)
+                if eValue > maxEval:
+                    maxEval = eValue
+                    bestChild = childNode  # 更新最大值的子节点
+                self.alpha = max(self.alpha, eValue)
+                if self.alpha >= self.beta:
+                    break
+            return bestChild, maxEval  # 返回最大值所对应的子节点和值
+        else:
+            minEval = +math.inf
+            bestChild = None  # 用于记录最小值的子节点
+            for child in node.sonNode:
+                childNode, eValue = self.alphaBetaSearch(child, depth - 1, True)
+                if eValue < minEval:
+                    minEval = eValue
+                    bestChild = childNode  # 更新最小值的子节点
+                self.beta = min(self.beta, eValue)
+                if self.beta <= self.alpha:
+                    break
+            return bestChild, minEval  # 返回最小值所对应的子节点和值
 
     def eval(self, matrix):
-        return getSmoothness(matrix) * self.__smoothWeight + getMonotonicity(matrix) * self.__monoWeight + math.log(
-            getEmptyCounts(matrix)) * self.__emptyWeight + getMaxValue(matrix) * self.__maxWeight
+        emptyCounts = math.fabs(self.getEmptyCounts(matrix))
+        if emptyCounts == 0:
+            return (self.getSmoothness(matrix) * self.smoothWeight +
+                    self.getMonotonicity(matrix) * self.monoWeight +
+                    self.getMaxValue(matrix) * self.maxWeight)
+        return (self.getSmoothness(matrix) * self.smoothWeight +
+                self.getMonotonicity(matrix) * self.monoWeight +
+                math.log(emptyCounts) * self.emptyWeight +
+                self.getMaxValue(matrix) * self.maxWeight)
 
-    def alphaBeta(self, matrix):
-       
+    # 暂不考虑
+    def machineOperator(self, node):
+        for child in node:
+            valueList = []
+            matrixIndex = []
+            for i in range(c.ROW):
+                for j in range(c.COLUMN):
+                    tmpMatrix = self.GAME.matrix
+                    if tmpMatrix[i][j] == 0:
+                        tmpMatrix.gridChange(i, j, 2)
+                        valueList.append(self.eval(tmpMatrix.getMatrix()))
+                        matrixIndex.append([i, j, 2])
+
+                        tmpMatrix.gridChange(i, j, 4)
+                        valueList.append(self.eval(tmpMatrix.getMatrix()))
+                        matrixIndex.append([i, j, 4])
+            index = valueList.index(min(valueList))
+            child.matrix = child.matrix.gridChange(matrixIndex[index][0], matrixIndex[index][1], matrixIndex[index][2])
+
+    @classmethod
+    def getSmoothness(cls, matrix):
+        # 计算每格平滑度
+        smoothness = 0
+        for i in range(c.ROW):
+            for j in range(c.COLUMN):
+                if matrix[i][j] != 0:
+                    value = math.log(matrix[i][j]) / math.log(2)
+                    # 该行处理
+                    tmpMatrix = matrix[i]
+
+        # 先对每一行进行单独求解
+        for i in range(c.ROW):
+            tmpMatrix = matrix[i]
+            for j in range(len(tmpMatrix) - 1):
+                if tmpMatrix[j] != 0:
+                    for k in range(j + 1, len(tmpMatrix)):
+                        if tmpMatrix[k] != 0:
+                            smoothness -= math.log(math.fabs(tmpMatrix[k] - tmpMatrix[j])) / math.log(2)
+                            break
+
+        # 对每一列求解
+        for j in range(c.COLUMN):
+            tmpMatrix = []
+            for i in range(c.ROW):
+                # 抽出矩阵的每一列
+                tmpMatrix.append(matrix[i][j])
+            for k in range(len(tmpMatrix) - 1):
+                if tmpMatrix[k] != 0:
+                    for t in range(k + 1, len(tmpMatrix)):
+                        if tmpMatrix[t] != 0:
+                            smoothness -= math.log(math.fabs(tmpMatrix[k] - tmpMatrix[t])) / math.log(2)
+                            break
+        return smoothness
+
+    @classmethod
+    def getMonotonicity(cls, matrix):
+        scoreList = [0, 0, 0, 0]
+        # Right
+        for i in range(c.ROW):
+            tmpMatrix = matrix[i]
+            for j in range(len(tmpMatrix) - 1):
+                if tmpMatrix[j] != 0:
+                    for k in range(j + 1, len(tmpMatrix)):
+                        if tmpMatrix[k] != 0:
+                            if tmpMatrix[j] >= tmpMatrix[k]:
+                                scoreList[0] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[j]) / math.log(2)
+                            else:
+                                scoreList[1] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[j]) / math.log(2)
+        # Left
+        for i in range(c.ROW):
+            tmpMatrix = matrix[i]
+            for j in range(len(tmpMatrix) - 1, 0, -1):
+                if tmpMatrix[j] != 0:
+                    for k in range(j - 1, len(tmpMatrix), -1):
+                        if tmpMatrix[k] != 0:
+                            if tmpMatrix[j] >= tmpMatrix[k]:
+                                scoreList[0] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[j]) / math.log(2)
+                            else:
+                                scoreList[1] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[j]) / math.log(2)
+
+        # Up
+        for j in range(c.COLUMN):
+            tmpMatrix = []
+            for i in range(c.ROW):
+                # 抽出矩阵的每一列
+                tmpMatrix.append(matrix[i][j])
+            for k in range(len(tmpMatrix) - 1):
+                if tmpMatrix[k] != 0:
+                    for t in range(k + 1, len(tmpMatrix)):
+                        if tmpMatrix[t] != 0:
+                            if tmpMatrix[k] >= tmpMatrix[t]:
+                                scoreList[2] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[t]) / math.log(2)
+                            else:
+                                scoreList[3] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[t]) / math.log(2)
+        # Down
+        for j in range(c.COLUMN):
+            tmpMatrix = []
+            for i in range(c.ROW):
+                # 抽出矩阵的每一列
+                tmpMatrix.append(matrix[i][j])
+            for k in range(len(tmpMatrix) - 1, 0, -1):
+                if tmpMatrix[k] != 0:
+                    for t in range(k - 1, len(tmpMatrix), -1):
+                        if tmpMatrix[t] != 0:
+                            if tmpMatrix[k] >= tmpMatrix[t]:
+                                scoreList[2] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[t]) / math.log(2)
+                            else:
+                                scoreList[3] += math.log(tmpMatrix[k]) / math.log(2) - math.log(
+                                    tmpMatrix[t]) / math.log(2)
+        return max(scoreList[0], scoreList[1]) + max(scoreList[2], scoreList[3])
+
+    @classmethod
+    def getEmptyCounts(cls, matrix):
+        counts = 0
+        for i in range(c.ROW):
+            for j in range(c.COLUMN):
+                if matrix[i][j] == 0:
+                    counts += 1
+        return counts
+
+    @classmethod
+    def getMaxValue(cls, matrix):
+        MAX = matrix[0][0]
+        for i in range(c.ROW):
+            for j in range(c.COLUMN):
+                if matrix[i][j] >= MAX:
+                    MAX = matrix[i][j]
+        return MAX
+
+
+class Node:
+    def __init__(self, matrix, depth):
+        self.sonNode = []
+        self.depth = depth
+        self.matrix = matrix
+
+        # 构造子节点
+        if self.depth <= 6:
+            self.sonNode.append(Node(self.matrix.right(), self.depth + 1))
+            self.sonNode.append(Node(self.matrix.left(), self.depth + 1))
+            self.sonNode.append(Node(self.matrix.up(), self.depth + 1))
+            self.sonNode.append(Node(self.matrix.down(), self.depth + 1))
+
+
+# Run the Code
+alphaGo = Ai()
+node, value = alphaGo.alphaBetaSearch(alphaGo.headNode, 6, True)
+if node is not None:
+    for i in range(4):
+        print(node.matrix.getMatrix()[i])
