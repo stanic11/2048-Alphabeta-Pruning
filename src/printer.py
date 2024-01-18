6f4de5e62ec64a88391be0bbbf926beca3434fee
YNAG <109913898+stanic11@users.noreply.github.com>
2024-01-18 11:52:39 +0800

    Delete src/printer.py

--- src/printer.py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QApplication,QPushButton,QWidget
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(0,0,1024,768)
-        # 初始化窗口大小
-        btn = QPushButton('开始游戏',self)
